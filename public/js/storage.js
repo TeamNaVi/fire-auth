968ebb81b4360be5c83a09942e573fa5e4d9cc3c
sori <smcdidcksthf@naver.com>
2022-05-17 00:48:27 +0900

    UI v1.0 : storage반영

--- public/js/storage.js
+++ public/js/storage.js
@@ -12,48 +12,77 @@ const fileTable1 = document.querySelector("#fileTable1"); //이미지
 const fileTable2 = document.querySelector("#fileTable2"); //가중치
 const loader = document.querySelector(".loader");
 
+// html element
+const page_title = document.getElementById("page-title");
+const li_Dashboard = document.getElementById("li_Dashboard");
+const li_Storage = document.getElementById("li_Storage");
+const li_Streaming = document.getElementById("li_Streaming");
+const li_DeepLearning = document.getElementById("li_DeepLearning");
+
+const logOut = document.getElementById("logOut");
+
 const auth = firebase.auth();
+
+logOut.addEventListener("click", () => {
+  //signOut() is a built in firebase function responsible for signing a user out
+  auth
+    .signOut()
+    .then(() => {
+      window.location.assign("../");
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+});
+
 // var global
 let fileBrowse = null;
 let urlDownload = null;
 let userUid = null;
 let userEmail = null;
-// sidebar
-// Requires jQuery
-$(document).on("click", ".js-menu_toggle.closed", function (e) {
-  e.preventDefault();
-  $(".list_load, .list_item").stop();
-  $(this).removeClass("closed").addClass("opened");
 
-  $(".side_menu").css({ left: "0px" });
-
-  var count = $(".list_item").length;
-  $(".list_load").slideDown(count * 0.6 * 100);
-  $(".list_item").each(function (i) {
-    var thisLI = $(this);
-    timeOut = 100 * i;
-    setTimeout(function () {
-      thisLI.css({
-        opacity: "1",
-        "margin-left": "0",
-      });
-    }, 100 * i);
-  });
+//Go to Dashboard page
+li_Dashboard.addEventListener("click", () => {
+  const user = auth.currentUser;
+  // admin@admin.admin uid
+  if (user.uid == "3RS7jsw7asP6Owe5pZomy5KGwkf1") {
+    window.location.assign("../dashboardAdmin");
+  } else {
+    window.location.assign("../dashboard");
+  }
 });
 
-$(document).on("click", ".js-menu_toggle.opened", function (e) {
-  e.preventDefault();
-  $(".list_load, .list_item").stop();
-  $(this).removeClass("opened").addClass("closed");
+//Go to Storage page
+li_Storage.addEventListener("click", () => {
+  const user = auth.currentUser;
+  // admin@admin.admin uid
+  if (user.uid == "3RS7jsw7asP6Owe5pZomy5KGwkf1") {
+    window.location.assign("../storageAdmin");
+  } else {
+    window.location.assign("../storage");
+  }
+});
 
-  $(".side_menu").css({ left: "-250px" });
+//Go to Streaming page
+li_Streaming.addEventListener("click", () => {
+  const user = auth.currentUser;
+  // admin@admin.admin uid
+  if (user.uid == "3RS7jsw7asP6Owe5pZomy5KGwkf1") {
+    window.location.assign("../streaming");
+  } else {
+    window.location.assign("../streaming");
+  }
+});
 
-  var count = $(".list_item").length;
-  $(".list_item").css({
-    opacity: "0",
-    "margin-left": "-20px",
-  });
-  $(".list_load").slideUp(300);
+//Go to Deep Learning page
+li_DeepLearning.addEventListener("click", () => {
+  const user = auth.currentUser;
+  // admin@admin.admin uid
+  if (user.uid == "3RS7jsw7asP6Owe5pZomy5KGwkf1") {
+    window.location.assign("../deepLearning");
+  } else {
+    window.location.assign("../deepLearning");
+  }
 });
 
 // upload.js
@@ -165,8 +194,11 @@ showListImage = function (no, fileName, fileLoc) {
 
   fileTable1.insertAdjacentHTML("beforeend", newHtml);
 };
+
 // init system
 auth.onAuthStateChanged((user) => {
+  page_title.innerHTML = user.displayName + "님의 Storage";
+
   userUid = user.uid;
   userEmail = user.email;
   getAllFiles(); //유저 정보가 들어왔을 때 리스팅(안 그러면 아무것도 안 뜸)
@@ -295,3 +327,57 @@ fileTable2.addEventListener("click", (e) => {
     }
   }
 });
+
+// UI
+let Dashboard = (() => {
+  let global = {
+    tooltipOptions: {
+      placement: "right",
+    },
+    tooltipOptionsBottom: {
+      placement: "bottom",
+    },
+    menuClass: ".c-menu",
+  };
+
+  // 리스트 item 활성화
+  // let menuChangeActive = (el) => {
+  //   let hasSubmenu = $(el).hasClass("has-submenu");
+  //   $(global.menuClass + " .is-active").removeClass("is-active");
+  //   $(el).addClass("is-active");
+
+  //   // 하위메뉴 있으면
+  //   // if (hasSubmenu) {
+  //   //   $(el).find("ul").slideDown();
+  //   // }
+  // };
+
+  let sidebarChangeWidth = () => {
+    let $menuItemsTitle = $("li .menu-item__title");
+
+    $("body").toggleClass("sidebar-is-reduced sidebar-is-expanded");
+    $(".hamburger-toggle").toggleClass("is-opened");
+
+    if ($("body").hasClass("sidebar-is-expanded")) {
+      $('[data-toggle="tooltip"]').tooltip("destroy");
+    } else {
+      $('[data-toggle="tooltip"]').tooltip(global.tooltipOptions);
+    }
+  };
+
+  return {
+    init: () => {
+      $(".js-hamburger").on("click", sidebarChangeWidth);
+
+      // item selected
+      // $(".js-menu li").on("click", (e) => {
+      //   menuChangeActive(e.currentTarget);
+      // });
+
+      $('[data-toggle="tooltip"]').tooltip(global.tooltipOptions);
+      $('[data-toggle="tooltipB"]').tooltip(global.tooltipOptionsBottom);
+    },
+  };
+})();
+
+Dashboard.init();
--- public/js/storageAdmin.js
+++ public/js/storageAdmin.js
@@ -13,48 +13,77 @@ const fileTable2 = document.querySelector("#fileTable2");
 const fileTable3 = document.querySelector("#fileTable3");
 const loader = document.querySelector(".loader");
 
+// html element
+const page_title = document.getElementById("page-title");
+const li_Dashboard = document.getElementById("li_Dashboard");
+const li_Storage = document.getElementById("li_Storage");
+const li_Streaming = document.getElementById("li_Streaming");
+const li_DeepLearning = document.getElementById("li_DeepLearning");
+
+const logOut = document.getElementById("logOut");
+
 const auth = firebase.auth();
+
+logOut.addEventListener("click", () => {
+  //signOut() is a built in firebase function responsible for signing a user out
+  auth
+    .signOut()
+    .then(() => {
+      window.location.assign("../");
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+});
+
 // var global
 let fileBrowse = null;
 let urlDownload = null;
 let userUid = null;
 let userEmail = null;
-// sidebar
-// Requires jQuery
-$(document).on("click", ".js-menu_toggle.closed", function (e) {
-  e.preventDefault();
-  $(".list_load, .list_item").stop();
-  $(this).removeClass("closed").addClass("opened");
 
-  $(".side_menu").css({ left: "0px" });
-
-  var count = $(".list_item").length;
-  $(".list_load").slideDown(count * 0.6 * 100);
-  $(".list_item").each(function (i) {
-    var thisLI = $(this);
-    timeOut = 100 * i;
-    setTimeout(function () {
-      thisLI.css({
-        opacity: "1",
-        "margin-left": "0",
-      });
-    }, 100 * i);
-  });
+//Go to Dashboard page
+li_Dashboard.addEventListener("click", () => {
+  const user = auth.currentUser;
+  // admin@admin.admin uid
+  if (user.uid == "3RS7jsw7asP6Owe5pZomy5KGwkf1") {
+    window.location.assign("../dashboardAdmin");
+  } else {
+    window.location.assign("../dashboard");
+  }
 });
 
-$(document).on("click", ".js-menu_toggle.opened", function (e) {
-  e.preventDefault();
-  $(".list_load, .list_item").stop();
-  $(this).removeClass("opened").addClass("closed");
+//Go to Storage page
+li_Storage.addEventListener("click", () => {
+  const user = auth.currentUser;
+  // admin@admin.admin uid
+  if (user.uid == "3RS7jsw7asP6Owe5pZomy5KGwkf1") {
+    window.location.assign("../storageAdmin");
+  } else {
+    window.location.assign("../storage");
+  }
+});
 
-  $(".side_menu").css({ left: "-250px" });
+//Go to Streaming page
+li_Streaming.addEventListener("click", () => {
+  const user = auth.currentUser;
+  // admin@admin.admin uid
+  if (user.uid == "3RS7jsw7asP6Owe5pZomy5KGwkf1") {
+    window.location.assign("../streaming");
+  } else {
+    window.location.assign("../streaming");
+  }
+});
 
-  var count = $(".list_item").length;
-  $(".list_item").css({
-    opacity: "0",
-    "margin-left": "-20px",
-  });
-  $(".list_load").slideUp(300);
+//Go to Deep Learning page
+li_DeepLearning.addEventListener("click", () => {
+  const user = auth.currentUser;
+  // admin@admin.admin uid
+  if (user.uid == "3RS7jsw7asP6Owe5pZomy5KGwkf1") {
+    window.location.assign("../deepLearning");
+  } else {
+    window.location.assign("../deepLearning");
+  }
 });
 
 // upload.js
@@ -174,7 +203,7 @@ showListUser = function (no, listEmail, listUid) {
     fileTable3.innerHTML = "";
     fileTable2.innerHTML = "";
     getAllFiles();
-    debug.log(userUid);
+
     inputInit();
   });
 };
@@ -222,8 +251,9 @@ showListImage = function (no, fileName, fileLoc) {
 };
 
 // init system
-
 auth.onAuthStateChanged((user) => {
+  page_title.innerHTML = user.displayName + "님의 Storage";
+
   if (user.email == "admin@admin.admin") {
     //관리자 계정으로 로그인 했을 때만 정보 표시
     getAllFiles();
@@ -359,3 +389,57 @@ fileTable3.addEventListener("click", (e) => {
     }
   }
 });
+
+// UI
+let Dashboard = (() => {
+  let global = {
+    tooltipOptions: {
+      placement: "right",
+    },
+    tooltipOptionsBottom: {
+      placement: "bottom",
+    },
+    menuClass: ".c-menu",
+  };
+
+  // 리스트 item 활성화
+  // let menuChangeActive = (el) => {
+  //   let hasSubmenu = $(el).hasClass("has-submenu");
+  //   $(global.menuClass + " .is-active").removeClass("is-active");
+  //   $(el).addClass("is-active");
+
+  //   // 하위메뉴 있으면
+  //   // if (hasSubmenu) {
+  //   //   $(el).find("ul").slideDown();
+  //   // }
+  // };
+
+  let sidebarChangeWidth = () => {
+    let $menuItemsTitle = $("li .menu-item__title");
+
+    $("body").toggleClass("sidebar-is-reduced sidebar-is-expanded");
+    $(".hamburger-toggle").toggleClass("is-opened");
+
+    if ($("body").hasClass("sidebar-is-expanded")) {
+      $('[data-toggle="tooltip"]').tooltip("destroy");
+    } else {
+      $('[data-toggle="tooltip"]').tooltip(global.tooltipOptions);
+    }
+  };
+
+  return {
+    init: () => {
+      $(".js-hamburger").on("click", sidebarChangeWidth);
+
+      // item selected
+      // $(".js-menu li").on("click", (e) => {
+      //   menuChangeActive(e.currentTarget);
+      // });
+
+      $('[data-toggle="tooltip"]').tooltip(global.tooltipOptions);
+      $('[data-toggle="tooltipB"]').tooltip(global.tooltipOptionsBottom);
+    },
+  };
+})();
+
+Dashboard.init();
